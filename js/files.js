013bf2482f9c723cddd023edf6d032c8eb7846a0
COHEAVEN <adhikarisijan444@gmail.com>
2026-06-18 21:36:29 +0545

    Refactor course data structure in files.js

--- js/files.js
+++ js/files.js
@@ -7,42 +7,30 @@ const FILES = {
   },
   "BCA-102": {
     notes: [
-     { title: "Chapter 1 - Introduction to Functions", file: "pdfs/BCA-102/notes/Introduction-to-Functions.pdf", date: "June 02, 2026" },
-      { title: "Chapter 2 - Introduction to Array", file: "pdfs/BCA-102/notes/Introduction to Array.pdf", date: "June 02, 2026"  },
-      { title: "Chapter 6-Control-Structure",file: "pdfs/BCA-102/notes/Chapter 6-Control-Structure.pdf", date: "June 02, 2026"  },
+      { title: "Chapter 1 - Introduction to Functions", file: "pdfs/BCA-102/notes/Introduction-to-Functions.pdf", date: "June 02, 2026" },
+      { title: "Chapter 2 - Introduction to Array", file: "pdfs/BCA-102/notes/Introduction to Array.pdf", date: "June 02, 2026" },
+      { title: "Chapter 6-Control-Structure", file: "pdfs/BCA-102/notes/Chapter 6-Control-Structure.pdf", date: "June 02, 2026" }
     ],
-    questionPapers: [
-     
-    ],
-    labReports: [
-     
-    ],
+    questionPapers: [],
+    labReports: [],
     assignments: [
-    { title: "Assignment 1 - Number Systems", file: "pdfs/BCA-101/assignments/Chapter 1-Assignment.pdf", date: "June 1, 2026" },
-  ]
+      { title: "Assignment 1 - Number Systems", file: "pdfs/BCA-101/assignments/Chapter 1-Assignment.pdf", date: "June 1, 2026" }
+    ]
   },
   "BCA-103": {
-    notes: [
-    
-    ],
+    notes: [],
     questionPapers: [],
     labReports: [],
     assignments: []
   },
   "BCA-104": {
-    notes: [
-     
-    ],
-    questionPapers: [
-     
-    ],
+    notes: [],
+    questionPapers: [],
     labReports: [],
     assignments: []
   },
   "BCA-105": {
-    notes: [
-    
-    ],
+    notes: [],
     questionPapers: [],
     labReports: [],
     assignments: []
@@ -50,79 +38,79 @@ const FILES = {
   "BCA-106": {
     notes: [],
     questionPapers: [],
-    labReports: [
-     
-    ]
+    labReports: [],
+    assignments: []
   },
   "BCA-151": {
     notes: [
-      { title: "UNIT-1 Fundamental Of Logic And Proof Techniques", file: "pdfs/BCA-151/notes/UNIT-1_Fundamental_of_Logic_and_Proof_Techniques.pdf", date: "Jun 18, 2026" }],
-    questionPapers: [
-      
+      { title: "UNIT-1 Fundamental Of Logic And Proof Techniques", file: "pdfs/BCA-151/notes/UNIT-1_Fundamental_of_Logic_and_Proof_Techniques.pdf", date: "Jun 18, 2026" }
     ],
+    questionPapers: [],
     labReports: [],
     assignments: []
   },
   "BCA-152": {
     notes: [
-   { title: "Microprocessor and Computer Architecture(Book)", file: "pdfs/BCA-152/notes/Microprocessor and Computer Architecture.pdf", date: "June 20, 2024" ,
-    assignments: [
-      { title: "Assignment 1 And 2[Chapter 1]", file: "pdfs/BCA-152/assignments/Assignment_1_and_2.pdf", date: "Jun 18, 2026" }]
-  },
-      { title: "Evolution of Microprocessors", file: "pdfs/BCA-152/notes/Evolution of Microprocessors.pdf", date: "June 12, 2024" },
+      { title: "Microprocessor and Computer Architecture(Book)", file: "pdfs/BCA-152/notes/Microprocessor and Computer Architecture.pdf", date: "June 20, 2024" },
+      { title: "Evolution of Microprocessors", file: "pdfs/BCA-152/notes/Evolution of Microprocessors.pdf", date: "June 12, 2024" }
     ],
     questionPapers: [],
-    labReports: [
-     
+    labReports: [],
+    assignments: [
+      { title: "Assignment 1 And 2[Chapter 1]", file: "pdfs/BCA-152/assignments/Assignment_1_and_2.pdf", date: "Jun 18, 2026" }
     ]
   },
   "BCA-153": {
+    notes: [],
+    questionPapers: [],
+    labReports: [],
+    assignments: [
+      { title: "Java ko assignments", file: "pdfs/BCA-153/assignments/20260607_211242.pdf", date: "Jun 7, 2026" }
+    ]
+  },
+  "BCA-154": {
+    notes: [],
+    questionPapers: [],
+    labReports: [],
+    assignments: []
+  },
+  "BCA-155": {
     notes: [
-    
+      { title: "Chapter 1-Introduction", file: "pdfs/BCA-155/notes/Chapter 1-Introduction.pdf", date: "June 02, 2026" },
+      { title: "Second Chapter", file: "pdfs/BCA-155/notes/Second_Chapter.pdf", date: "Jun 18, 2026" }
     ],
     questionPapers: [
-     
+      { title: "Unit Test Set A[Chapter 1]", file: "pdfs/BCA-155/question-papers/Unit_Test_Set_A.pdf", date: "Jun 18, 2026" },
+      { title: "Unit Test Set B[Chapter 1]", file: "pdfs/BCA-155/question-papers/Unit_Test_Set_B.pdf", date: "Jun 18, 2026" }
     ],
-    labReports: [
-
- ]
-  ,
+    labReports: [],
     assignments: [
-      { title: "Java ko assignments", file: "pdfs/BCA-153/assignments/20260607_211242.pdf", date: "Jun 7, 2026" }]
+      { title: "Chapter 1-Assignment", file: "pdfs/BCA-155/assignments/Chapter 1-Assignment.pdf", date: "June 02, 2026" },
+      { title: "Chapter 2 Assignment", file: "pdfs/BCA-155/assignments/Chapter_2_Assignment.pdf", date: "Jun 18, 2026" }
+    ]
   },
-  "BCA-154": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-155": { notes: [
-     { title: "Chapter 1-Introduction", file: "pdfs/BCA-155/notes/Chapter 1-Introduction.pdf", date: "June 02, 2026" ,
-    assignments: [
-      { title: "Chapter 2 Assignment", file: "pdfs/BCA-155/assignments/Chapter_2_Assignment.pdf", date: "Jun 18, 2026" ,
-    notes: [
-      { title: "Second Chapter", file: "pdfs/BCA-155/notes/Second_Chapter.pdf", date: "Jun 18, 2026" ,
-    question-papers: [{ title: "Unit Test Set A[Chapter 1,
-      { title: "Unit Test Set B[Chapter 1]", file: "pdfs/BCA-155/question-papers/Unit_Test_Set_B.pdf", date: "Jun 18, 2026" }]", file: "pdfs/BCA-155/question-papers/Unit_Test_Set_A.pdf", date: "Jun 18, 2026" }]
-  }]
-  }]
+  "BCA-156": {
+    notes: [],
+    questionPapers: [],
+    labReports: [],
+    assignments: []
   },
-  ], questionPapers: [], labReports: [],
-assignments: [ { title: "Chapter 1-Assignment", file: "pdfs/BCA-155/assignments/Chapter 1-Assignment.pdf", date: "June 02, 2026" },
-
-] },
-  "BCA-156": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-201": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-202": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-203": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-204": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-205": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-206": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-251": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-252": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-253": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-254": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-255": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-256": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-301": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-302": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-303": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-304": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-305": { notes: [], questionPapers: [], labReports: [] },
-  "BCA-306": { notes: [], questionPapers: [], labReports: [] },
+  "BCA-201": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-202": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-203": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-204": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-205": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-206": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-251": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-252": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-253": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-254": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-255": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-256": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-301": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-302": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-303": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-304": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-305": { notes: [], questionPapers: [], labReports: [], assignments: [] },
+  "BCA-306": { notes: [], questionPapers: [], labReports: [], assignments: [] }
 };
