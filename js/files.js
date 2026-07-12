933b7717d7ba17e915c35faed74d0ecd597de2d2
COHEAVEN <adhikarisijan444@gmail.com>
2026-07-12 17:29:57 +0545

    Add files via upload

--- js/files.js
+++ js/files.js
@@ -8,13 +8,13 @@ const FILES = {
   "BCA-102": {
     notes: [
       { title: "Chapter 1 - Introduction to Functions", file: "pdfs/BCA-102/notes/Introduction-to-Functions.pdf", date: "June 02, 2026" },
-      { title: "Chapter 2 - Introduction to Array", file: "pdfs/BCA-102/notes/Introduction%20to%20Array.pdf", date: "June 02, 2026" },
-      { title: "Chapter 6-Control-Structure", file: "pdfs/BCA-102/notes/Chapter%206-Control-Structure.pdf", date: "June 02, 2026" }
+      { title: "Chapter 2 - Introduction to Array", file: "pdfs/BCA-102/notes/Introduction_to_Array.pdf", date: "June 02, 2026" },
+      { title: "Chapter 6-Control-Structure", file: "pdfs/BCA-102/notes/Chapter_6-Control-Structure.pdf", date: "June 02, 2026" }
     ],
     "question-papers": [],
     "lab-reports": [],
-    "assignments": [
-      { title: "Assignment 1 - Number Systems", file: "pdfs/BCA-102/assignments/Chapter%201-Assignment.pdf", date: "June 1, 2026" }
+    assignments: [
+      { title: "Assignment 1 - Number Systems", file: "pdfs/BCA-102/assignments/Chapter_1-Assignment.pdf", date: "June 1, 2026" }
     ]
   },
   "BCA-103": {
@@ -51,8 +51,8 @@ const FILES = {
   },
   "BCA-152": {
     notes: [
-      { title: "Microprocessor and Computer Architecture(Book)", file: "pdfs/BCA-152/notes/Microprocessor%20and%20Computer%20Architecture.pdf", date: "June 20, 2026" },
-      { title: "Evolution of Microprocessors", file: "pdfs/BCA-152/notes/Evolution%20of%20Microprocessors.pdf", date: "June 12, 2026" }
+      { title: "Microprocessor and Computer Architecture(Book)", file: "pdfs/BCA-152/notes/Microprocessor_and_Computer_Architecture.pdf", date: "June 20, 2026" },
+      { title: "Evolution of Microprocessors", file: "pdfs/BCA-152/notes/Evolution_of_Microprocessors.pdf", date: "June 12, 2026" }
     ],
     "question-papers": [],
     "lab-reports": [],
@@ -66,9 +66,6 @@ const FILES = {
     "lab-reports": [
       { title: "Lab Report 2 and Assignment", file: "pdfs/BCA-153/lab-reports/LAB-2.pdf", date: "Jun 27, 2026" }
     ],
-
-      
-      { title: "OOP IN JAVA[LAB 2] Solution", file: "pdfs/BCA-153/lab-reports/OOP_IN_JAVA_LAB_2_-Solution.pdf", date: "Jul 12, 2026" }
     assignments: [
       { title: "Java ko assignments", file: "pdfs/BCA-153/assignments/20260607_211242.pdf", date: "Jun 7, 2026" }
     ]
@@ -81,7 +78,7 @@ const FILES = {
   },
   "BCA-155": {
     notes: [
-      { title: "Chapter 1-Introduction", file: "pdfs/BCA-155/notes/Chapter%201-Introduction.pdf", date: "June 02, 2026" },
+      { title: "Chapter 1-Introduction", file: "pdfs/BCA-155/notes/Chapter_1-Introduction.pdf", date: "June 02, 2026" },
       { title: "Second Chapter", file: "pdfs/BCA-155/notes/Second_Chapter.pdf", date: "Jun 18, 2026" },
       { title: "Third Chapter Note", file: "pdfs/BCA-155/notes/Third_Chapter_Note.pdf", date: "Jul 9, 2026" }
     ],
@@ -91,7 +88,7 @@ const FILES = {
     ],
     "lab-reports": [],
     assignments: [
-      { title: "Chapter 1-Assignment", file: "pdfs/BCA-155/assignments/Chapter%201-Assignment.pdf", date: "June 02, 2026" },
+      { title: "Chapter 1-Assignment", file: "pdfs/BCA-155/assignments/Chapter_1-Assignment.pdf", date: "June 02, 2026" },
       { title: "Chapter 2 Assignment", file: "pdfs/BCA-155/assignments/Chapter_2_Assignment.pdf", date: "Jun 18, 2026" }
     ]
   },
